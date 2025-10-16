b8192ddfdea2fc67f69ee86b79d3cad54af3c997
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-10-16 15:00:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (38.48%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -1,20 +1,13 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        String x1 = Integer.toBinaryString(x);
-        String y1 = Integer.toBinaryString(y);
-        int count=0;
-        int maxlen=Math.max(x1.length(),y1.length());
-        while(x1.length()<maxlen){
-            x1="0"+x1;
-        }
-        while(y1.length()<maxlen){
-            y1="0"+y1;
-        }
-        for(int i=0;i<maxlen;i++){
-            if(x1.charAt(i)!=y1.charAt(i)){
-                count++;
-            }
+        int count =0;
+        int xor = x^y;
+      
+        while(xor!=0){
+           count+=xor&1;
+           xor>>=1;
         }
         return count;
+
     }
 }
