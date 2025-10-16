c9a22d076cc4b2038b1cb93889b0580a2d2db639
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-10-16 15:16:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (11.61%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,15 +1,11 @@
 class Solution {
     public int findComplement(int num) {
-        String num1 = Integer.toBinaryString(num);
-        String flip="";
-        for(int i=0;i<num1.length();i++){
-            if(num1.charAt(i)=='1'){
-                flip+="0";
-            }else{
-                flip+="1";
-            }
+        int mask =0;
+        int temp = num;
+        while(temp!=0){
+            mask=(mask<<1)|1;
+            temp>>=1;
         }
-        int flip1 = Integer.parseInt(flip,2);
-        return flip1;
+        return num^mask;
     }
 }
