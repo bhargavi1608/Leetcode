3c904ffac81988cec585b0680e25238111688fa3
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-01-06 13:43:30 +0530

    Time: 9 ms (64.42%) | Memory: 49.5 MB (11.96%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -15,25 +15,49 @@
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
+        // int level =1;
+        // Queue<TreeNode> q = new LinkedList<>();
+
+        // q.add(root);
+        // int maxSum =Integer.MIN_VALUE;
+        // int sum=0;
+        // int maxLevel =0;
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode current = q.poll();
+        //         sum+=current.val;
+        //         if(current.left!=null) q.add(current.left);
+        //         if(current.right!=null) q.add(current.right);
+
+        //     }
+        //     if(sum>maxSum){
+        //         maxSum = sum;
+        //         maxLevel = level;
+        //     }
+        //     sum=0;
+        //     level++;
+        // }
+        // return maxLevel;
+
         int level =1;
         Queue<TreeNode> q = new LinkedList<>();
-
         q.add(root);
-        int maxSum =Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
         int sum=0;
-        int maxLevel =0;
+        int maxLevel=0;
+
         while(!q.isEmpty()){
             int size = q.size();
             for(int i=0;i<size;i++){
-                TreeNode current = q.poll();
-                sum+=current.val;
-                if(current.left!=null) q.add(current.left);
-                if(current.right!=null) q.add(current.right);
-
+               TreeNode current = q.poll();
+               sum+=current.val;
+               if(current.left!=null) q.add(current.left);
+               if(current.right!=null) q.add(current.right);
             }
             if(sum>maxSum){
-                maxSum = sum;
-                maxLevel = level;
+                maxSum=sum;
+                maxLevel=level;
             }
             sum=0;
             level++;
