0dfb66d201d780764dd9a6c8f12c9894572eaeac
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-03-17 11:06:13 +0530

    Time: 3 ms (41.74%) | Memory: 43.3 MB (97.55%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,15 +1,10 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        for(int num:arr){
-            map.put(num,map.getOrDefault(num,0)+1);
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int val:arr){
+            map.put(val,map.getOrDefault(val,0)+1);
         }
-        Set<Integer> set = new HashSet<>();
-        for(int val:map.values()){
-            if(!set.add(val)){
-                return false;
-            }
-        }
-        return true;
+        Set<Integer> set = new HashSet<>(map.values());
+        return set.size() == map.size();
     }
 }
