bac7f98d8f81d9775876d8e35b4b15027e142097
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-06-07 17:29:19 +0530

    Time: 5 ms (14.62%) | Memory: 43.5 MB (15.21%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        List<Character> openlist = Arrays.asList('(','[','{');
-        List<Character> closelist = Arrays.asList(')',']','}');
-        for(char ch : s.toCharArray()){
-            if(openlist.contains(ch)){
-                stack.push(ch);
-            }else if(closelist.contains(ch)){
-                if(stack.isEmpty()) return false;
-                char top = stack.pop();
-                if(openlist.indexOf(top)!=closelist.indexOf(ch)){
+        List<Character> open =  Arrays.asList('(','{','[');
+        List<Character> close =  Arrays.asList(')','}',']');
+        Stack<Character> st = new Stack<>();
+        for(char c:s.toCharArray()){
+            if(open.contains(c)){
+                st.push(c);
+            }else{
+                if(st.isEmpty()){
                     return false;
+                }else{
+                    char top = st.pop();
+                    if(c==')'&& top!='(' || c==']' && top!='[' || c=='}' && top!= '{'){
+                        return false;
+                    }
                 }
+                
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
