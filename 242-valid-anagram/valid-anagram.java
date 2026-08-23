c439809f1a295d3da5ff5e175e818d025ef87ee8
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-08-23 12:27:56 +0530

    Time: 4 ms (83.18%) | Memory: 46.3 MB (47.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] c1 = s.toCharArray();
-        char[] c2 = t.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        return Arrays.equals(c1,c2);
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+        Arrays.sort(sa);
+        Arrays.sort(ta);
+        return Arrays.equals(sa,ta);
     }
 }
