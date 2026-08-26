8fd54e132d6d04be1d98b723aabb8f6b6446f0b5
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-08-26 18:01:53 +0530

    Time: 6 ms (87.78%) | Memory: 43.8 MB (93.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,33 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        // sliding window
-        if(s1.length()>s2.length()){
-            return false;
-        }
-        int[] s1freq = new int[26];
-        int[] window = new int[26];
-        for(int i=0;i<s1.length();i++){
-            s1freq[s1.charAt(i)-'a']++;
-            window[s2.charAt(i)-'a']++;
-        }
-        if(matches(s1freq,window)){
+       if(s1.length()>s2.length()) return false;
+       int[] s1freq = new int[26];
+       int[] s2freq = new int[26];
+       for(int i=0;i<s1.length();i++){
+        s1freq[s1.charAt(i)-'a']++;
+       }
+       int k = s1.length();
+       for(int i=0;i<k;i++){
+        s2freq[s2.charAt(i)-'a']++;
+       }
+       if(Arrays.equals(s1freq,s2freq)){
+         return true;
+       }
+       for(int i=k;i<s2.length();i++){
+          s2freq[s2.charAt(i)-'a']++;
+          s2freq[s2.charAt(i-k)-'a']--;
+          if(Arrays.equals(s1freq,s2freq)){
             return true;
-        }
-        for(int i=s1.length();i<s2.length();i++){
-            window[s2.charAt(i)-'a']++;
-            window[s2.charAt(i-s1.length())-'a']--;
-            if(matches(s1freq,window)){
-                return true;
-            }
-        }
-        return false;
-    }
-    public static boolean matches(int[] a, int[] b){
-        for(int i=0;i<26;i++){
-            if(a[i]!=b[i]){
-                return false;
-            }
-        }
-        return true;
+          }
+       }
+       return false;
+
     }
 }
