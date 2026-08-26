48636a814079bf7f5984a61a8139489b3ef1a848
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-08-26 22:51:40 +0530

    Time: 4 ms (36.57%) | Memory: 43.3 MB (57.67%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
+        ArrayList<Character> l1 = new ArrayList<>(Arrays.asList('(','{','['));
+        ArrayList<Character> l2 = new ArrayList<>(Arrays.asList(')','}',']'));
         for(char c:s.toCharArray()){
-            if(c=='('||c=='{'||c=='['){
+            if(l1.contains(c)){
                 st.push(c);
             }else{
-                if(!st.isEmpty()){
-                    char top = st.pop();
-                    if(c==')' && top!='(' || c=='}' && top!='{' || c==']' && top!='['){
-                        return false;
-                    }
-                }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top = st.pop();
+                if(c==')' && top!='(' || c==']' && top!='[' || c=='}' && top!='{'){
                     return false;
                 }
             }
