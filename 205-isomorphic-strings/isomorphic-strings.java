48809f4ba2bf1bcf475edb023c47b75789335caa
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-09-09 18:35:46 +0530

    Time: 15 ms (49.05%) | Memory: 45.2 MB (5.77%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,12 +3,13 @@ class Solution {
         if(s.length()!=t.length()) return false;
         List<Integer> l1 = new ArrayList<>();
         List<Integer> l2 = new ArrayList<>();
-        for(char c:s.toCharArray()){
-            l1.add(s.indexOf(c));
+        for(int i=0;i<s.length();i++){
+            l1.add(s.indexOf(s.charAt(i)));
         }
-        for(char c:t.toCharArray()){
-            l2.add(t.indexOf(c));
+        for(int i=0;i<t.length();i++){
+            l2.add(t.indexOf(t.charAt(i)));
         }
-        return l1.equals(l2);
+        return  l1.equals(l2);
+
     }
 }
