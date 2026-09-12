71dddf8b4d70cf242241ec98cf7720c1668b1973
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-09-12 12:01:47 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (39.29%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,34 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
-        int i=0;
-        int len=s.length();
-        // remove trailing spaces
-        while(i<len && s.charAt(i)==' '){
-            i++;
-        }
-        if(i==len) return 0;
-        // handles sign
+      
+        String res ="";
+        s=s.trim();
+        if(s.length()==0) return 0;
         int sign =1;
-        if(s.charAt(i)=='+' || s.charAt(i)=='-'){
-            if(s.charAt(i)=='-'){
-               sign =-1;
-            }
+        int i=0;
+
+        // Hamndle sign
+        if(s.charAt(i)=='-'){
+            sign = -1;
+            i++;
+        }else if(s.charAt(i)=='+'){
             i++;
         }
-
-        // conversion to int with overflow check
-
-        int res =0;
-        while(i<len && Character.isDigit(s.charAt(i))){
+        int num=0;
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
             int digit = s.charAt(i)-'0';
-            if(res> Integer.MAX_VALUE/10 || res==Integer.MAX_VALUE/10 && digit>7){
-                return sign==1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if(num>Integer.MAX_VALUE/10 || num==Integer.MAX_VALUE/10 && digit>7){
+                return (sign==1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            res = res*10+digit;
+            num=num*10+digit;
             i++;
         }
-        
-        return sign*res;
-
+        return num*sign;
     }
 }
