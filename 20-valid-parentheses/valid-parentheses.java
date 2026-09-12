a329c0407890381025579d812673d819f41c2941
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-09-12 23:18:50 +0530

    Time: 4 ms (36.28%) | Memory: 43.3 MB (40.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        ArrayList<Character> l1 = new ArrayList<>(Arrays.asList('(','{','['));
-        ArrayList<Character> l2 = new ArrayList<>(Arrays.asList(')','}',']'));
+        Stack<Character>stack = new Stack<>();
+        ArrayList<Character> open = new ArrayList<>(Arrays.asList('[','{','('));
+       
         for(char c:s.toCharArray()){
-            if(l1.contains(c)){
-                st.push(c);
+            if(open.contains(c)){
+                stack.push(c);
             }else{
-                if(st.isEmpty()){
+                if(stack.isEmpty()){
                     return false;
                 }
-                char top = st.pop();
-                if(c==')' && top!='(' || c==']' && top!='[' || c=='}' && top!='{'){
-                    return false;
+                else{
+                    char top = stack.pop();
+                    if(c==')' && top!='(' || c==']'&& top!='[' || c=='}' && top!='{'){
+                        return false;
+                    }
                 }
             }
+
         }
-        return st.isEmpty();
+        return stack.isEmpty();
+
     }
 }
